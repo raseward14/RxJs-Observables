1b578f124c1ea4efe7f7e625667a7da3a2f2fa47
raseward14 <raseward14@gmail.com>
2026-03-03 17:22:22 -0700

    implemented the interval observable from rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, DestroyRef, OnInit } from '@angular/core';
+
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef)
 
+  ngOnInit() {
+    const subscription = interval(1000).subscribe({
+      next: (val) => console.log(val)
+    });
+  }
 }
